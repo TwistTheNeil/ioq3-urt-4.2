20b164cb47359222bd96d57a21eddb8388be063c
Lojjik Braughler <llbraughler@gmail.com>
2012-08-07 03:14:26 -0400

    initial commit

---
+++ build/release-linux-x86_64/ded/sv_snapshot.d
@@ -0,0 +1,11 @@
+build/release-linux-x86_64/ded/sv_snapshot.o: code/server/sv_snapshot.c \
+ code/server/server.h code/server/../qcommon/q_shared.h \
+ code/server/../qcommon/q_platform.h \
+ code/server/../qcommon/surfaceflags.h code/server/../qcommon/qcommon.h \
+ code/server/../qcommon/../qcommon/cm_public.h \
+ code/server/../qcommon/../qcommon/qfiles.h \
+ code/server/../game/g_public.h code/server/../game/bg_public.h \
+ code/server/../qcommon/vm_local.h code/server/../qcommon/q_shared.h \
+ code/server/../qcommon/qcommon.h code/server/../sys/sys_local.h \
+ code/server/../sys/../qcommon/q_shared.h \
+ code/server/../sys/../qcommon/qcommon.h
